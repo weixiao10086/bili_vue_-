8ebc2995fd8c55492bbf5ef734fa718309340854
weixiao <2216694426@qq.com>
2022-07-21 12:24:19 +0800

    1

--- admin/src/main.js
+++ admin/src/main.js
@@ -14,7 +14,7 @@ Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 Vue.prototype.$http=axios.create({
-  baseURL:'http://localhost:3000/'
+  baseURL:'http://81.68.198.249:3000/'
 })
 // Vue.use(VueAxios, axios)
 
